6a9e6a0d29dd4a15d2e2d3d82d1f5eeb8d82855d
cartonni <nicolas.carton@natixis.com>
2021-05-28 10:57:40 +0200

    corrections

--- src/main/java/com/wildcodeschool/tricount/controller/ContactController.java
+++ src/main/java/com/wildcodeschool/tricount/controller/ContactController.java
@@ -1,5 +1,8 @@
 package com.wildcodeschool.tricount.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +21,9 @@ public class ContactController {
     
     @GetMapping("/contacts")
     public String getAll(Model model) {
-        contactService.getAll(model);
+        List<Contact> contacts = new ArrayList<Contact>();
+        contacts = contactService.getAll();
+        model.addAttribute("contacts", contacts);
         return "contacts";
     }
 
