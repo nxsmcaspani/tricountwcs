3e1077d33d39d4d1e78b9e92d7952994a980f108
cartonni <nicolas.carton@natixis.com>
2021-06-23 15:57:51 +0200

    premiere version balance

--- src/main/java/com/wildcodeschool/tricount/dto/BalanceExpenseDto.java
+++ src/main/java/com/wildcodeschool/tricount/dto/BalanceExpenseDto.java
@@ -3,61 +3,12 @@ package com.wildcodeschool.tricount.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.wildcodeschool.tricount.entity.Contact;
-import com.wildcodeschool.tricount.entity.Expense;
-import com.wildcodeschool.tricount.entity.ExpenseList;
-
 public class BalanceExpenseDto {
 
     private String name;
     private List<ContactForBalanceDto> lstContacts = new ArrayList<ContactForBalanceDto>();
     private Float total = 0f;
     private boolean balanceOk = false;
-    private List<Expense> lstExpenseToDo = new ArrayList<Expense>();
-    
-    public BalanceExpenseDto(ExpenseList exp) {
-        this.name = exp.getName();
-        for (Contact ct : exp.getContacts()) {
-            lstContacts.add(new ContactForBalanceDto(ct.getId(), ct.getName(), ct.getEmail()));
-        }
-        for (Expense dep : exp.getExpensesList()) {
-            this.total += dep.getAmount();
-            for (ContactForBalanceDto ctDto : lstContacts) {
-                if (ctDto.getId()== dep.getOwner().getId()) {
-                    ctDto.setAmountSpend(ctDto.getAmountDue() + dep.getAmount());
-                    // TODO: si le owner pas présent dans les bénéficiaires, retirer le commentaire et faire +1 aussi aux bénefs
-                    // ctDto.setAmountDue(dep.getAmount() / dep.getBeneficiaries().size() + 1);
-                }
-                for (Contact ct : dep.getBeneficiaries()) {
-                    if (ctDto.getId() == ct.getId()) {
-                        ctDto.setAmountDue(dep.getAmount() / dep.getBeneficiaries().size());
-                    }
-                }
-            }
-        }
-        balanceOk = checkBalanceOk();
-        lstExpenseToDo = calculateBalance();
-    }
-    
-    private boolean checkBalanceOk() {
-        if (lstContacts.size() <2) {
-            return true;
-        }
-        for (int i = 1; i < lstContacts.size(); i++) {
-            if (lstContacts.get(i).getAmountDue() != lstContacts.get(i-1).getAmountDue()) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private List<Expense> calculateBalance() {
-        List<Expense> lstExpense = new ArrayList<Expense>();
-        // TODO
-        
-        
-        return lstExpense;
-    }
     
     public String getName() {
         return name;
@@ -89,14 +40,6 @@ public class BalanceExpenseDto {
 
     public void setBalanceOk(boolean aBalanceOk) {
         balanceOk = aBalanceOk;
-    }
-
-    public List<Expense> getLstExpenseToDo() {
-        return lstExpenseToDo;
-    }
-
-    public void setLstExpenseToDo(List<Expense> aLstExpenseToDo) {
-        lstExpenseToDo = aLstExpenseToDo;
     }
     
 }
--- src/main/java/com/wildcodeschool/tricount/service/BalanceExpenseListService.java
+++ src/main/java/com/wildcodeschool/tricount/service/BalanceExpenseListService.java
@@ -1,9 +1,16 @@
 package com.wildcodeschool.tricount.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wildcodeschool.tricount.dto.BalanceExpenseDto;
+import com.wildcodeschool.tricount.dto.ContactForBalanceDto;
+import com.wildcodeschool.tricount.entity.Contact;
+import com.wildcodeschool.tricount.entity.Expense;
+import com.wildcodeschool.tricount.entity.ExpenseList;
 
 @Service
 public class BalanceExpenseListService {
@@ -13,12 +20,55 @@ public class BalanceExpenseListService {
     
     
     public BalanceExpenseDto getDtoBalanceExpense(int idList) {
-        BalanceExpenseDto balDto = new BalanceExpenseDto(expenseListService.getExpenseList(idList));
+        BalanceExpenseDto balDto = convExpenseListToBalanceDto(expenseListService.getExpenseList(idList));
         return balDto; // expenseListService.convertFromEntityToDto(idList);
     }
     
     
-    
+    /**
+     * Conversion d'une ExpenseList en BalanceExpenseDto pour page Balance
+     * @param ExpenseList
+     * @return BalanceExpenseDto
+     */
+    private BalanceExpenseDto convExpenseListToBalanceDto(ExpenseList exp) {
+        BalanceExpenseDto balDto = new BalanceExpenseDto();
+        balDto.setName(exp.getName());
+        List<ContactForBalanceDto> lstCt = new ArrayList<ContactForBalanceDto>();
+        for (Contact ct : exp.getContacts()) {
+            lstCt.add(new ContactForBalanceDto(ct.getId(), ct.getName(), ct.getEmail()));
+        }
+        balDto.setLstContacts(lstCt);
+        
+        for (Expense dep : exp.getExpensesList()) {
+            balDto.setTotal(balDto.getTotal() + dep.getAmount());
+            for (ContactForBalanceDto ctDto : balDto.getLstContacts()) {
+                if (ctDto.getId()== dep.getOwner().getId()) {
+                    ctDto.setAmountSpend(ctDto.getAmountSpend() + dep.getAmount());
+                    // TODO: si le owner pas présent dans les bénéficiaires, retirer le commentaire et faire +1 aussi aux bénefs
+                    // ctDto.setAmountDue(dep.getAmount() / dep.getBeneficiaries().size() + 1);
+                }
+                for (Contact ct : dep.getBeneficiaries()) {
+                    if (ctDto.getId() == ct.getId()) {
+                        ctDto.setAmountDue(ctDto.getAmountDue() + (dep.getAmount() / dep.getBeneficiaries().size()));
+                    }
+                }
+            }
+        }
+        balDto.setBalanceOk(checkBalanceOk(balDto.getLstContacts()));
+        return balDto;
+    }
+
+    private boolean checkBalanceOk(List<ContactForBalanceDto> lstContacts) {
+        if (lstContacts.size() <2) {
+            return true;
+        }
+        for (int i = 1; i < lstContacts.size(); i++) {
+            if (lstContacts.get(i).getAmountDue() != lstContacts.get(i-1).getAmountDue()) {
+                return false;
+            }
+        }
+        return true;
+    }
     
     
     
